192b9f9851cac10deecd451a53bdc7fd8028958f
Sergey Georgiev <sgrgv_art@mail.ru>
2024-01-16 21:25:00 +0300

    Создание запроса для получения выполенных задач

---
+++ server/controllers/toDo.js
@@ -0,0 +1,19 @@
+import toDo from "../models/toDo.js";
+
+const getTodos = async (req, res) => {
+  try {
+    const todos = await toDo.find({ status: "выполнено" }).sort("-createdAt");
+
+    if (!todos) {
+      return res.json({
+        message: "Задач нет",
+      });
+    }
+
+    res.json({
+      todos,
+    });
+  } catch (error) {
+    res.json({ error, message: "Что-то пошло не так" });
+  }
+};
