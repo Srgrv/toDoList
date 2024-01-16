ac4824b4d79df3cf530767c9604d6f09f9f075c8
Sergey Georgiev <sgrgv_art@mail.ru>
2024-01-16 20:59:31 +0300

    Запуск сервера и подключение к mongoDb

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express"; // создание приложения
+import mongoose from "mongoose"; // подключение к базе данных
+import dotenv from "dotenv"; // шифрование в формате .env
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 3002;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+
+app.use(express.json());
+
+async function start() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@todolist.texezdj.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+    );
+
+    app.listen(PORT, () => {
+      console.log(`Hello, server was started on the PORT: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
