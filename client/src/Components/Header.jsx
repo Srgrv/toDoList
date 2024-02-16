20c016cb46e6d36e35dcde8fcc20e9c318513a25
Sergey Georgiev <sgrgv_art@mail.ru>
2024-02-16 11:08:46 +0300

    Создан Header

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100vw",
+        height: "50px",
+        backgroundColor: "#000000",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white",
+      }}
+    >
+      My Todos
+    </div>
+  );
+};
+
+export default Header;
