b588ca9f3c055dbe5b7c50b62aa63831e9908ca5
Sergey Georgiev <sgrgv_art@mail.ru>
2024-02-16 11:12:53 +0300

    Добавлены роуты

--- server/routes/toDo.js
+++ server/routes/toDo.js
@@ -2,20 +2,34 @@ import { Router } from "express";
 
 //controllers
 import {
-  getCompletedTodos,
+  // getCompletedTodos,
   getInProgressTodos,
   getPendingTodos,
+  get_completedTodos,
   createToDo,
+  put_updateTodo,
+  put_comleteTodo,
+  put_incompleteTodo,
+  delete_removeTodo,
 } from "../controllers/toDo.js";
 
 const router = new Router();
 
 //get
-router.get("/getCompletedTodos", getCompletedTodos);
+// router.get("/getCompletedTodos", getCompletedTodos);
 router.get("/getInProgressTodos", getInProgressTodos);
 router.get("/getPendingTodos", getPendingTodos);
+router.get("/getCompletedTodos", get_completedTodos);
+
+//post
 router.post("/createToDo", createToDo);
 
-//create
+//put
+router.put("/updateTodo", put_updateTodo);
+router.put("/completeTodo", put_comleteTodo);
+router.put("/incompleteTodo", put_incompleteTodo);
+
+//delete
+router.delete("/removeTodo/:todoId", delete_removeTodo);
 
 export default router;
