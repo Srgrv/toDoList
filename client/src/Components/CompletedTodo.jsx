2d92d5cf73fce51583e5d0bef55f42b9d1af2ac5
Sergey Georgiev <sgrgv_art@mail.ru>
2024-02-16 11:08:05 +0300

    Создан компонент

---
+++ client/src/Components/CompletedTodo.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+//css
+import style from "./PendingTodo.module.css";
+
+//icon
+import { FaCheck } from "react-icons/fa";
+
+//components
+import CustomCheckbox from "./CustomCheckbox";
+
+const CompletedTodo = (props, { item, icon, checkIcon }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className={style.todoListItem}>
+      <div className={style.todoListItem_wrapper}>
+        <div>
+          {/* <FaCheck /> */}
+          {/* {checkIcon} */}
+          <CustomCheckbox
+            checked={props.item.checked}
+            id={props.item._id}
+            status={props.item.status}
+          />
+        </div>
+        <div className={style.todo}>
+          <h3>{props.item.title}</h3>
+          <p>{props.item.description}</p>
+        </div>
+        <div>{props.icon}</div>
+      </div>
+    </div>
+  );
+};
+
+export default CompletedTodo;
