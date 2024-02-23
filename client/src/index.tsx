6f7137322e622200bfc4dbeea56b91b29bfb0dc9
Sergey Georgiev <sgrgv_art@mail.ru>
2024-02-23 15:35:41 +0300

    Добавлено as HTMLElement

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,7 +8,9 @@ import { Provider } from "react-redux";
 //store
 import store from "./store/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(
   <BrowserRouter>
